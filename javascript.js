8638a7b28eb2d6107fe405a226f7873125e1274a
Arda Arman <ardaarman99@gmail.com>
2018-08-06 17:17:08 +0300

    -added scrollreveal options

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // Easy scroll animations for web and mobile browsers. https://scrollrevealjs.org/
 
 window.sr = ScrollReveal();
-sr.reveal(".card");
-sr.reveal(".testimonial");
+sr.reveal(".card", {delay: 200});
+sr.reveal(".testimonial", {delay: 200});
